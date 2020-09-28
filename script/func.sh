0ad8ee39f3645e08be8ca1e55058cf5f96d10f03
zhangjun <zhangjun@hangsheng.com.cn>
2020-09-28 17:36:23 +0800

    [bug修复]N/A [详细描述] update [影响范围] [日期] [提交人] 章军

--- script/func.sh
+++ script/func.sh
@@ -6,6 +6,10 @@ f ()
 	OpenSource=vendor/nxp-opensource/
 	img=out/target/product/mek_8q
 	UUU=uuu/
+	QNXPre=/home/z/workspace/QNX-Space/qnxSource
+	qnxSrc=src/hardware
+	qnxUUU=images/emmc/imx8-uuu
+	
 
 	# echo "cd fast version"                                 
 	case $1 in                                             
@@ -18,22 +22,50 @@ f ()
 		m4)                                                   
 			cd ${ProjectPre}${OpenSource}${M4}
 			;;                         
-		bm4)                                                                 
-			cd ${ProjectPre}${OpenSource}${}
-			;;                                                                       
-		image)                      
+		img)                      
 			cd ${ProjectPre}${img}
 			;;                                                                                            
 		u)
 			cd ${WorkSpace}${UUU}
 			;;
-		cam):                                                                                 
+		cam)
 			cd $CAMARE                                                                                    
 			;;                                                                                            
+		note):
+			cd /home/z/Nutstore\ Files
+			;;
+		qnx)
+			cd ${QNXPre}
+			;;
+		qsrc)
+			cd ${QNXPre}${qnxSrc}
+			;;
+		qu)
+			cd ${QNXPre}${qnxUUU}
+			;;
 		*)        
 			echo "Badly aegument"                                                                         
 			;;                                                                                            
 	esac 
+}
+
+Mount()
+{
+	remoteDir=/home/danny/i8QM/imx-p9.0.0_2.1.1-auto-ga/android_buil
+	localDir=/home/z/workspace/IMX8QM-Andoird/source
+
+	case $1 in
+		a)
+			# fusermount -u ${localDir}
+			/home/z/workspace/bin/mountAndoird
+			;;
+		rm)
+			sudo fusermount -u ${localDir}
+			;;
+		*)
+			echo "wrong argument"
+			;;
+	esac
 }
 
 # ctags and cscopr
@@ -44,12 +76,12 @@ Sync()
 {
 	case $1 in
 		-b)
-			ctags -R .
-			cscope -Rbq
+			sudo ctags -R .
+			sudo cscope -Rbq
 			;;
 		-c)
-			rm tags
-			rm cscope.in.out cscope.out cscope.po.out
+			sudo rm tags
+			sudo rm cscope.in.out cscope.out cscope.po.out
 			;;
 	esac
 }
@@ -74,4 +106,36 @@ AndPre()
 }
 
 
+IMX8ENV()
+{
+	export ARCH=arm64
+	export CROSS_COMPILE=/home/z/workspace/IMX8QM-Andoird/android_build/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+	export ARMGCC_DIR=/home/z/workspace/IMX8QM-Andoird/android_build/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/
+}
+
+proxy ()
+{
+	export ALL_PROXY=socks5://127.0.0.1:7890
+	export https_proxy=http://127.0.0.1:7890
+	export http_proxy=http://127.0.0.1:7890
+	export all_proxy=socks5://127.0.0.1:7891
+
+# git config --global https.proxy http://127.0.0.1:7890
+# git config --global https.proxy https://127.0.0.1:7891
+	git config --global http.proxy 'socks5://127.0.0.1:7890' 
+	git config --global https.proxy 'socks5://127.0.0.1:7891'
+}
+
+unproxy ()
+{
+	unset ALL_PROXY="socks5://127.0.0.1:7890"
+	unset https_proxy="http://127.0.0.1:7890"
+	unset http_proxy="http://127.0.0.1:7890"
+	unset all_proxy="socks5://127.0.0.1:7891"
+
+# git config --global https.proxy http://127.0.0.1:7890
+# git config --global https.proxy https://127.0.0.1:7891
+	git config --global --unset http.proxy
+	git config --global --unset https.proxy
+}
 
